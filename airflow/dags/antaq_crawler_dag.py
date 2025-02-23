9a15ad3dfdd614becf2819ba6e5edd39d5e3c975
Rafael Panegassi <rm.panegassi@gmail.com>
2025-02-23 15:45:11 -0300

    fix: dag crawler to raw bucket

--- airflow/dags/antaq_crawler_dag.py
+++ airflow/dags/antaq_crawler_dag.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-import os
+import runpy
 
 # Default arguments for the DAG
 default_args = {
@@ -12,24 +12,24 @@ default_args = {
     'retries': 1,
 }
 
-# Define the DAG
+# Define a DAG
 with DAG(
     'antaq_crawler_dag',
     default_args=default_args,
     description='Run ANTAQ crawler to download and process ZIP files into MinIO',
-    schedule_interval='0 12 1 * *',  # Runs at noon (12:00) on the 1st of every month
-    start_date=datetime(2023, 1, 1),  # Adjust start date as needed
-    catchup=False,  # Prevents backfilling for past dates
+    schedule_interval='0 12 1 * *',  # Roda ao meio-dia do primeiro dia de cada mês
+    start_date=datetime(2023, 1, 1),  # Ajuste a data de início conforme necessário
+    catchup=False,  # Impede a execução retroativa das tarefas
     tags=['antaq', 'monthly', 'crawler'],
 ) as dag:
 
     def run_antaq_crawler():
-        # Use the absolute path where jobs is mounted in the container
+        # Caminho absoluto onde o script está montado no container
         script_path = '/opt/airflow/jobs/crawlers/crawler_antaq.py'
-        with open(script_path, 'r') as file:
-            exec(file.read())
+        # Executa o script como se estivesse sendo chamado como o módulo principal
+        runpy.run_path(script_path, run_name="__main__")
 
-    # Define the task
+    # Define a task
     antaq_crawler_task = PythonOperator(
         task_id='run_antaq_crawler',
         python_callable=run_antaq_crawler,
